5226ad3eb388dcc73c90ae977b8d05dbfb8be076
student <student@loaner.fios-router.home>
2017-08-17 22:45:36 -0400

    initail changes

--- FMB Payroll Project.R
+++ FMB Payroll Project.R
@@ -12,34 +12,46 @@ library(genderdata)
 
 # Load data
 df1 <- read.csv("bank_payroll.csv")
-unique(df1$employee_id)
 
 # remove duplicates
 df2 <- unique.data.frame(df1)
 
 
-# data manipulation
+# data manipulation and adding a gender column (with First name)
 df3 <- df2 %>% 
   mutate(First_name = str_split_fixed(name, " ", 3)[,1],
          Middle_name =str_split_fixed(name, " ",3)[,2],
          Last_name = str_split_fixed(name, " ", 3)[,3]) %>% 
   select(employee_id, First_name, Middle_name, Last_name, basic_salary, 
-         employee_pension_contribution, employer_pension_contribution)
+         employee_pension_contribution, employer_pension_contribution) 
+
+# Predict gender using  first name
+df4 <- df3 %>% 
+  rowwise() %>% 
+  do(results = gender(.$First_name, method = "ssa")) %>% 
+  do(bind_rows(.$results))
+
+# predict gender using middle name
+df5 <- df3 %>% 
+  rowwise() %>% 
+  do(df4 = gender(.$Middle_name, method = "ssa")) %>% 
+  do(bind_rows(.$df4))
+ 
+# Joining first name with name in Results from gender function
+df6 <- inner_join(df3,df4, by = c("First_name"="name"))
+
+# Joining middle name with name in Results from Gender function
+df7 <- inner_join(df3,df5, by = c("First_name" = "name"))
+
+# Combine the two data frames
+df8 <- rbind(df6,df7)
+
 
 # data visualization
 ggplot(df3, aes(x=basic_salary))+ geom_histogram(bins = 100)
 
-# Load names dataframe
-Babynames <-babynames
-Babynames_clean <- Babynames %>% 
-  rename(First_name = name) %>% 
-  select(sex, First_name) %>% 
-  unique()
 
-# Uning the Babynames package
-df4 <- left_join(df3, Babynames_clean, by ="First_name")
-rm(Babynames1)
 
 # Using the Gender package
-Gender_names<- as.data.frame(gender(df3$First_name))
-
+Gender_names<- as.data.frame(gender(df3$First_nam))
+rm(Babynames, Babynames_clean)
